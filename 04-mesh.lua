f2216d64cf26681fc9ebaf23f6da788bbbe4fd3b
Cloud Wu <cloudwu@gmail.com>
2019-03-12 17:12:30 +0800

    example 04

--- 04-mesh.lua
+++ 04-mesh.lua
@@ -1,34 +1,36 @@
-local ant = require "ant"
-local util = require "ant.util"
-local math3d = require "ant.math"
+package.cpath = "bin/?.dll"
+
+local iup = require "iuplua"
 local bgfx = require "bgfx"
+local util = require "util"
+local math3d = require "math3d"
 
-canvas = iup.canvas {}
-
-dlg = iup.dialog {
-  canvas,
-  title = "04-mesh",
-  size = "HALFxHALF",
+local ctx = {
+	canvas = iup.canvas {},
 }
 
-local ctx = {}
+local ms = math3d.new()
+
+local dlg = iup.dialog {
+	ctx.canvas,
+	title = "04-mesh",
+	size = "HALFxHALF",
+}
 
 local time = 0
 local function mainloop()
-	math3d.reset()
+	math3d.reset(ms)
 	bgfx.touch(0)
 	time = time + 0.01
-	local timev = math3d.vector():pack(time)
-	bgfx.set_uniform(ctx.u_time, timev)
-	local mtx = math3d.matrix():rotmat(0, time)
+	bgfx.set_uniform(ctx.u_time, {time,0,0,0})
+	local mtx = ms( { type = "srt", r = {0, time, 0} }, "m")
 	bgfx.set_transform(mtx)
 	bgfx.set_state(ctx.state)
 	util.meshSubmit(ctx.mesh, 0, ctx.prog)
 	bgfx.frame()
 end
 
-local function init(canvas)
-	ant.init { nwh = iup.GetAttributeData(canvas,"HWND") }
+function ctx.init()
 	bgfx.set_view_clear(0, "CD", 0x303030ff, 1, 0)
 --	bgfx.set_debug "ST"
 
@@ -41,40 +43,25 @@ local function init(canvas)
 		CULL = "CCW",
 		MSAA = true,
 	}
-
-	ant.mainloop(mainloop)
 end
 
-function canvas:resize_cb(w,h)
-	if init then
-		init(self)
-		init = nil
-	end
+function ctx.resize(w,h)
 	ctx.width = w
 	ctx.height = h
 	bgfx.reset(w,h, "v")
 
-	local viewmat = math3d.matrix "view"
-	local projmat = math3d.matrix "proj"
-	viewmat:lookatp( 0,1,-2.5, 0,1,0)
-	projmat:projmat(60, ctx.width/ctx.height, 0.1, 100)
+	local viewmat = ms( {0,1,-2.5,1}, {0,1,0, 1}, "lm")
+	local projmat = ms( { type = "mat", fov = 60, aspect = w/h , n = 0.1, f = 100 }, "m")
+
 	bgfx.set_view_transform(0, viewmat, projmat)
 	bgfx.set_view_rect(0, 0, 0, ctx.width, ctx.height)
 end
 
-function canvas:action(x,y)
-	mainloop()
-end
-
+util.init(ctx)
 dlg:showxy(iup.CENTER,iup.CENTER)
 dlg.usersize = nil
+util.run(mainloop)
 
--- to be able to run this script inside another context
-if (iup.MainLoopLevel()==0) then
-  iup.MainLoop()
-  iup.Close()
-  bgfx.destroy(ctx.u_time)
-  bgfx.destroy(ctx.prog)
-  util.meshUnload(ctx.mesh)
-  ant.shutdown()
-end
+--  bgfx.destroy(ctx.u_time)
+--  bgfx.destroy(ctx.prog)
+--  util.meshUnload(ctx.mesh)
