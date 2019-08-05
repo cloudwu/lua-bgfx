74e5e2d56ba24ee061ffea22f11aafb9a607867f
Cloud Wu <cloudwu@gmail.com>
2019-08-05 14:55:31 +0800

    fix #9

--- luabgfximgui.cpp
+++ luabgfximgui.cpp
@@ -1544,8 +1544,8 @@ winSetNextWindowPos(lua_State *L) {
 
 static int
 winSetNextWindowSize(lua_State *L) {
-	float x = luaL_checkinteger(L, 1);
-	float y = luaL_checkinteger(L, 2);
+	float x = luaL_checknumber(L, 1);
+	float y = luaL_checknumber(L, 2);
 	ImGuiCond cond = get_cond(L, 3);
 	ImGui::SetNextWindowSize(ImVec2(x,y), cond);
 	return 0;
@@ -1553,18 +1553,18 @@ winSetNextWindowSize(lua_State *L) {
 
 static int
 winSetNextWindowSizeConstraints(lua_State *L) {
-	float min_w = luaL_checkinteger(L, 1);
-	float min_h = luaL_checkinteger(L, 2);
-	float max_w = luaL_checkinteger(L, 3);
-	float max_h = luaL_checkinteger(L, 4);
+	float min_w = luaL_checknumber(L, 1);
+	float min_h = luaL_checknumber(L, 2);
+	float max_w = luaL_checknumber(L, 3);
+	float max_h = luaL_checknumber(L, 4);
 	ImGui::SetNextWindowSizeConstraints(ImVec2(min_w,min_h), ImVec2(max_w, max_h));
 	return 0;
 }
 
 static int
 winSetNextWindowContentSize(lua_State *L) {
-	float x = luaL_checkinteger(L, 1);
-	float y = luaL_checkinteger(L, 2);
+	float x = luaL_checknumber(L, 1);
+	float y = luaL_checknumber(L, 2);
 	ImGui::SetNextWindowContentSize(ImVec2(x,y));
 	return 0;
 }
@@ -1659,8 +1659,8 @@ cSpacing(lua_State *L) {
 
 static int
 cDummy(lua_State *L) {
-	float x = luaL_checkinteger(L, 1);
-	float y = luaL_checkinteger(L, 2);
+	float x = luaL_checknumber(L, 1);
+	float y = luaL_checknumber(L, 2);
 	ImGui::Dummy(ImVec2(x,y));
 	return 0;
 }
@@ -1832,10 +1832,10 @@ uSetColorEditOptions(lua_State *L) {
 
 static int
 uPushClipRect(lua_State *L) {
-	float left = luaL_checkinteger(L, 1);
-	float top = luaL_checkinteger(L, 2);
-	float right = luaL_checkinteger(L, 3);
-	float bottom = luaL_checkinteger(L, 4);
+	float left = luaL_checknumber(L, 1);
+	float top = luaL_checknumber(L, 2);
+	float right = luaL_checknumber(L, 3);
+	float bottom = luaL_checknumber(L, 4);
 	bool intersect_with_current_clip_rect = lua_toboolean(L, 5);
 	ImGui::PushClipRect(ImVec2(left,top), ImVec2(right, bottom), intersect_with_current_clip_rect);
 	return 0;
